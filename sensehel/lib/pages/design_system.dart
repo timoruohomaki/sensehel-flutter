86307e34f388dd7f635e270bc6bcf3f8c4fcb3e4
timoruohomaki <timo@ruohomaki.fi>
2024-06-22 11:00:38 +0300

    initiated project

---
+++ sensehel/lib/pages/design_system.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+// color swatches created with Material palette generator
+
+MaterialColor sensePrimaryBlue = const MaterialColor(
+  0xFF325a75,
+  <int, Color>{
+    50: Color(0xFFdef1ff),
+    100: Color(0xFFbedaef),
+    200: Color(0xFFa0bfd8),
+    300: Color(0xFF80a4c1),
+    400: Color(0xFF6890af),
+    500: Color(0xFF4f7d94),
+    600: Color(0xFF416e8c),
+    700: Color(0xFF325e75),
+    800: Color(0xFF24475f),
+    900: Color(0xFF113247),
+  },
+);
+
+MaterialColor senseComplementaryBrown = const MaterialColor(
+  0x754d32,
+  <int, Color>{
+    50: Color(0xFFf9e8da),
+    100: Color(0xFFdfc7b8),
+    200: Color(0xFFc1a392),
+    300: Color(0xFFa2806b),
+    400: Color(0xFF8c664e),
+    500: Color(0xFF754d32),
+    600: Color(0xFF6a442c),
+    700: Color(0xFF5b3823),
+    800: Color(0xFF4d2b1c),
+    900: Color(0xFF3d1e12),
+  },
+);
---
+++ sensehel/lib/pages/sensors_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SensorsPage extends StatelessWidget {
+  const SensorsPage({super.key});
+
+  @override
+
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+
+
+}
