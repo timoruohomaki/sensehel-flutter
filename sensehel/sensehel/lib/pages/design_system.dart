6d1c44e900039fcdb66fc4709003207a35dcd90a
timoruohomaki <timo@ruohomaki.fi>
2024-06-22 17:04:25 +0300

    added design_system.dart
    custom MaterialColor schemes

---
+++ sensehel/sensehel/lib/pages/design_system.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+// color swatches created with Material palette generator
+
+MaterialColor sensePrimaryBlue = const MaterialColor(
+  0xFF325a75,
+  <int, Color>{
+    50: Color(0xFFdef1ff),
+    100: Color(0xFFbedaef),
+    200: Color(0xFFa0bfd8),
+    300: Color(0xFF80a4c1),
+    400: Color(0xFF6890af),
+    500: Color(0xFF4f7d94),
+    600: Color(0xFF416e8c),
+    700: Color(0xFF325e75),
+    800: Color(0xFF24475f),
+    900: Color(0xFF113247),
+  },
+);
+
+MaterialColor senseComplementaryBrown = const MaterialColor(
+  0xFF754d32,
+  <int, Color>{
+    50: Color(0xFFf9e8da),
+    100: Color(0xFFdfc7b8),
+    200: Color(0xFFc1a392),
+    300: Color(0xFFa2806b),
+    400: Color(0xFF8c664e),
+    500: Color(0xFF754d32),
+    600: Color(0xFF6a442c),
+    700: Color(0xFF5b3823),
+    800: Color(0xFF4d2b1c),
+    900: Color(0xFF3d1e12),
+  },
+);
+
+MaterialColor senseOnBackground = const MaterialColor(
+  0xFF515253,
+  <int, Color>{
+    50: Color(0xFFfcfdfe),
+    100: Color(0xFFf7f8f9),
+    200: Color(0xFFf2f3f4),
+    300: Color(0xFFedeeef),
+    400: Color(0xFFcdcfd0),
+    500: Color(0xFFb0b1b3),
+    600: Color(0xFF969788),
+    700: Color(0xFF717273),
+    800: Color(0xFF515253),
+    900: Color(0xFF2f2f30),
+  },
+);
+
+// TODO: senseOrange
+
+MaterialColor senseSuccess = const MaterialColor(
+  0xFF00df70,
+  <int, Color>{
+    50: Color(0xFFe5fbeb),
+    100: Color(0xFFc1f4ce),
+    200: Color(0xFF95edaf),
+    300: Color(0xFF5de78c),
+    400: Color(0xFF00df70),
+    500: Color(0xFF00d752),
+    600: Color(0xFF00c747),
+    700: Color(0xFF00b33a),
+    800: Color(0xFF00a12c),
+    900: Color(0xFF008016),
+  },
+);
+
+MaterialColor senseFailure = const MaterialColor(
+  0xFFdf3a00,
+  <int, Color>{
+    50: Color(0xFFfde9e6),
+    100: Color(0xFFffcbb9),
+    200: Color(0xFFffaa8c),
+    300: Color(0xFFff875e),
+    400: Color(0xFFff6c39),
+    500: Color(0xFFff5111),
+    600: Color(0xFFfb4b0c),
+    700: Color(0xFFed4305),
+    800: Color(0xFFdf3a00),
+    900: Color(0xFFc72c00),
+  },
+);
